e385ff57310e6382093ad5227ac06a15e1dd2600
Bo-Jhang <TimeStringAlpha@gmail.com>
2014-06-13 01:06:02 -0700

    Initial Commit

---
+++ iBeaconCollectTests/iBeaconCollectTests.m
@@ -0,0 +1,34 @@
+//
+//  iBeaconCollectTests.m
+//  iBeaconCollectTests
+//
+//  Created by Bo Jhang Ho on 6/13/14.
+//  Copyright (c) 2014 Bo Jhang Ho. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iBeaconCollectTests : XCTestCase
+
+@end
+
+@implementation iBeaconCollectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
